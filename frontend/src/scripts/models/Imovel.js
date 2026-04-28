5eae72378a90f2714c4fc77a2da9aa7e5c7e0fe1
Eduarda <eduarda.damacena@upe.br>
2026-04-28 20:39:15 -0300

    inserindo backend flask

---
+++ frontend/src/scripts/models/Imovel.js
@@ -0,0 +1,12 @@
+class Imovel {
+    constructor(id, titulo, preco, tipo, imagem, quarto, banheiro, descricao) {
+        this.id = id ? id : crypto.randomUUID();
+        this.titulo = titulo;
+        this.preco = Number(preco);
+        this.tipo = tipo;
+        this.imagem = imagem;
+        this.descricao = descricao;
+        this.quarto = Number(quarto);
+        this.banheiro = Number(banheiro);
+    }
+}
